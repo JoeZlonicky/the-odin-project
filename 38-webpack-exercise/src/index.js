1fbfbe999bf931724c784ec3cb84768cb3033a94
JoeZlonicky <joe.zlonicky@gmail.com>
2024-05-19 19:08:11 -0700

    Update webpack exercise

--- 38-webpack-exercise/src/index.js
+++ 38-webpack-exercise/src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import myName from './myName';
 
-function component() {
+function webpackComponent() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello,', 'webpack!'], ' ');
     return element;
 }
 
-document.body.appendChild(component());
+function nameComponent() {
+    const element = document.createElement('div');
+    element.textContent = myName('Joe');
+    return element;
+}
+
+document.body.appendChild(webpackComponent());
+document.body.appendChild(nameComponent());
