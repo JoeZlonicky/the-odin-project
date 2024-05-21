8500760096ec44d53417197fe3ceb13f6166e40f
JoeZlonicky <joe.zlonicky@gmail.com>
2024-05-21 09:18:37 -0700

    Continue webpack exercise

--- 38-webpack-exercise/src/index.js
+++ 38-webpack-exercise/src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
+import Squiggle from './images/squiggle.png';
+import XMLNotes from './data/notes.xml';
+import CSVNotes from './data/notes.csv';
+import Tom from './data/tom.yaml';
 
 function webpackComponent() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello,', 'webpack!'], ' ');
+    element.classList.add('hello');
     return element;
+}
+
+function squiggleComponent() {
+    const img = new Image();
+    img.src = Squiggle;
+    return img;
 }
 
 function nameComponent() {
@@ -15,3 +27,7 @@ function nameComponent() {
 
 document.body.appendChild(webpackComponent());
 document.body.appendChild(nameComponent());
+document.body.appendChild(squiggleComponent());
+console.log(XMLNotes);
+console.log(CSVNotes);
+console.log(Tom);
---
+++ 38-webpack-exercise/webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const yaml = require('yamljs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'head'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.yaml$/i,
+                type: 'json',
+                parser: {
+                    parse: yaml.parse,
+                },
+            },
+        ]
+    },
+};
