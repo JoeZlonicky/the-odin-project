3398f4b5d97a7e87457a87fc2ef291253f7a0050
Joe Zlonicky <joe.zlonicky@gmail.com>
2024-05-29 21:09:16 -0700

    Refactor from CommonJS to ESM

---
+++ 41-dynamic-ui-interactions/webpack.common.js
@@ -0,0 +1,35 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const entry = './src/index.js';
+export const output = {
+  filename: 'bundle.js',
+  path: resolve(__dirname, 'dist'),
+  clean: true,
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    scriptLoading: 'defer',
+  }),
+];
+export const module = {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+  ],
+};
