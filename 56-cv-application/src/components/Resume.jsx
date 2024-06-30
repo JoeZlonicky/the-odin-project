c6be504ac53d854d09ada790b1b8ed6175ebba78
JoeZlonicky <joe.zlonicky@gmail.com>
2024-06-30 14:25:58 -0700

    More styling

--- 56-cv-application/src/components/Resume.jsx
+++ 56-cv-application/src/components/Resume.jsx
@@ -1,21 +1,30 @@
+import '../style/Resume.css';
+import TrayContainer from './TrayContainer';
+
 function Resume({ info, setIsEditing }) {
   return (
-    <div>
-      <h2>{info.name}</h2>
-      <div>
+    <div className="resume">
+      <h2 className="resume__name">{info.name}</h2>
+      <div className="resume__contact">
         {info.phoneNumber} | {info.email} | {info.linkedIn}
       </div>
       <h3>Education</h3>
-      <div>{info.certification}</div>
+      <hr></hr>
       <div>
-        {info.schoolName}, {info.startYear}-{info.graduationYear}
+        <strong>{info.certification}</strong>
+      </div>
+      <div>
+        <i>{info.schoolName}</i>, {info.startYear}-{info.graduationYear}
       </div>
       <h3>Work Experience</h3>
+      <hr></hr>
       <div>
-        {info.positionTitle} @ {info.companyName}, {info.startDate}-{info.endDate}
+        <strong>{info.positionTitle}</strong> @ <i>{info.companyName}</i>, {info.startDate}-{info.endDate}
       </div>
       <div>{info.description}</div>
-      <button onClick={() => setIsEditing(true)}>Edit</button>
+      <TrayContainer>
+        <button onClick={() => setIsEditing(true)}>Edit</button>
+      </TrayContainer>
     </div>
   );
 }
