855d2dd04f2b2eb62443ccec6a13e05f70c91136
JoeZlonicky <joe.zlonicky@gmail.com>
2024-07-30 10:53:12 -0700

    Rewrite basic site using express

--- 60-basic-informational-site/app.js
+++ 60-basic-informational-site/app.js
@@ -1,28 +1,29 @@
-import fs from 'node:fs';
-import http from 'node:http';
-import url from 'node:url';
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-http
-  .createServer((req, res) => {
-    const path = url.parse(req.url, true).pathname;
-    let fileName;
-    let isValidPath = true;
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const options = {
+  root: __dirname,
+};
 
-    if (path === '/') {
-      fileName = './index.html';
-    } else if (path === '/contact') {
-      fileName = './contact.html';
-    } else if (path === '/about') {
-      fileName = './about.html';
-    } else {
-      fileName = './404.html';
-      isValidPath = false;
-    }
+app.get('/', (_req, res) => {
+  res.sendFile('index.html', options);
+});
 
-    fs.readFile(fileName, (err, data) => {
-      res.writeHead(isValidPath ? 200 : 400, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(8080);
+app.get('/contact', (_req, res) => {
+  res.sendFile('contact.html', options);
+});
+
+app.get('/about', (_req, res) => {
+  res.sendFile('about.html', options);
+});
+
+app.use((_req, res) => {
+  res.status(404);
+  res.sendFile('404.html', options);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
